30afd3c4d5f3179a6e2f5afeea99d4c52c177c12
Benjamin Koehler <benjaminkoehler89@gmail.com>
2017-11-23 18:47:29 +0100

    @NFL_Predict fix test case numbering

--- NFL Predict/PredictPlays5_test.py
+++ NFL Predict/PredictPlays5_test.py
@@ -22,20 +22,20 @@ with open("2012_nfl_pbp_data_rp.csv") as f:
     header = next(reader) # Jump the header
     for row in reader:    
         # row = gameid, qtr, min, sec, off, def, down, togo, ydline, description, offscore, defscore, season,,,
-        if(int(row[6]) > 0 and int(row[6]) <=3):
-            down = int(row[6])
-            distance = int(row[7])
-            spot = int(row[8])
-            play = str(row[9])
-            time = 60.0 - ( (int(row[2])) - (1.0 - int(row[3])/60))
-            pointDiff = int(row[10]) - int(row[11])
-            # use roughly 80% of data to train / 20% of data to test 
-            if(random.random() < .80):
-                gameSituations_train.append([down, distance, spot, time, pointDiff])
-                playCalls_train.append(play)
-            else:
-                gameSituations_test.append([down, distance, spot, time, pointDiff])
-                playCalls_test.append(play)
+    
+        down = int(row[6])
+        distance = int(row[7])
+        spot = int(row[8])
+        play = str(row[9])
+        time = 60.0 - ( (int(row[2])) - (1.0 - int(row[3])/60))
+        pointDiff = int(row[10]) - int(row[11])
+        # use roughly 80% of data to train / 20% of data to test 
+        if(random.random() < .80):
+            gameSituations_train.append([down, distance, spot, time, pointDiff])
+            playCalls_train.append(play)
+        else:
+            gameSituations_test.append([down, distance, spot, time, pointDiff])
+            playCalls_test.append(play)
             
 f.close()
             
